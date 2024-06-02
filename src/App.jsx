4a690c0f03d2ac4e5f4c0ef702cb8723aa2198a3
MeirJC <meir84@gmail.com>
2024-06-02 23:57:09 +0300

    Added class carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import SearchParams from './SearchParams';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Details from './Details';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import AdoptedPetContext from './AdoptedPetContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,21 +13,27 @@ const queryClient = new QueryClient({
     }
   }
 });
+
 const App = () => {
+  const adoptedPet = useState(null);
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/" className="top-header" id="top-header">
-            Adopt Me!
-          </Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/" className="top-header" id="top-header">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+      </BrowserRouter>
+    </>
   );
 };
 
