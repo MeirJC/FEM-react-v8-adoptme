c47e70ca8eb785d8dd38b61f31396677ae12894c
MeirJC <meir84@gmail.com>
2024-06-02 23:32:44 +0300

    added tanstack/qurey and updated fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import SearchParams from './SearchParams';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Details from './Details';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+});
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/" className="top-header" id="top-header">
-          Adopt Me!
-        </Link>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/" className="top-header" id="top-header">
+            Adopt Me!
+          </Link>
+        </header>
 
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,38 @@
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import fetchPet from './fetchPet';
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>{id}</h2>;
+  const results = useQuery(['details', id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+  console.log(pet);
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+      {pet?.images?.length > 0 && (
+        <div className="pet-images">
+          {pet.images.map((image, index) => (
+            <img key={index} src={image} alt={pet.name} width={256} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Details;
