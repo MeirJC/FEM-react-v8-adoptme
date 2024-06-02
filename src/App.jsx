498f6edabf2386bcf06639b66312387f7745919c
MeirJC <meir84@gmail.com>
2024-06-02 23:09:13 +0300

    Added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import SearchParams from './SearchParams';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Details from './Details';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="top-header" id="top-header">
-        Adopt Me!
-      </h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/" className="top-header" id="top-header">
+          Adopt Me!
+        </Link>
+      </header>
+
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
+{
+  /* <div>
+<h1 className="top-header" id="top-header">
+  Adopt Me!
+</h1>
+<SearchParams />
+</div> */
+}
